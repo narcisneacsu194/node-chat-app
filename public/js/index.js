3b685505f7e314613fa47f865a51e62cb64446eb
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-17 16:22:37 +0200

    Added capability to broadcast a message that a user creates.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'randomUser2@example.com',
-    text: 'Random text from user.'
-  });
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -12,24 +12,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on.',
-    createdAt: 123
-  });
-
-  socket.emit('newMessage', {
-    from: 'randomName@example.com',
-    text: 'Random text.',
-    createdAt: 234
-  });  
-
   socket.on('disconnect', () => {
     console.log('User disconnected.');
   });
 
   socket.on('createMessage', (message) => {
     console.log('New message received from user', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 });
 
