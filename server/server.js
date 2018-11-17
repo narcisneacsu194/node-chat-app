1de6cdb926ab88eff5d0c0e1bc530f009ebfad90
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-17 19:26:01 +0200

    Continuation of the last commit.

--- server/server.js
+++ server/server.js
@@ -16,13 +16,30 @@ io.on('connection', (socket) => {
     console.log('User disconnected.');
   });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
-    console.log('New message received from user', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 
