c275bfaa64d84147ec0a1aeb499fdb2a92e5f1b9
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-20 19:59:36 +0200

    Added css for the chat app.

--- public/js/index.js
+++ public/js/index.js
@@ -20,10 +20,14 @@ socket.on('newMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function(){});
+    text: messageTextbox.val()
+  }, function(){
+    messageTextbox.val('');
+  });
 });
 
 var locationButton = jQuery('#send-location');
@@ -32,12 +36,16 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
@@ -45,6 +53,7 @@ locationButton.on('click', function(){
 socket.on('newLocationMessage', function(message){
   var listItem = jQuery('<li></li>');
   var anchor = jQuery('<a></a>');
+  listItem.text(`${message.from}: `);
   anchor.text('My current location');
   anchor.attr('href', message.url);
   anchor.attr('target', '_blank');
