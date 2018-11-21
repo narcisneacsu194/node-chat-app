4c262b37f81767f26b31625984c41726047822e0
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-21 10:49:11 +0200

    Added autoscroll to the messages container.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children();
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to server');
 });
@@ -17,6 +32,7 @@ socket.on('newMessage', function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e){
@@ -62,4 +78,5 @@ socket.on('newLocationMessage', function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
